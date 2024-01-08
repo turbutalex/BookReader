cec55779011778ec2ab0e42bdc0d49aaf92859dc
Vila Florin-Bogdan <vfir3248@scs.ubbcluj.ro>
2024-01-08 13:27:23 +0200

    Added dynamic pictures on the main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,26 +4,54 @@ import { Readable } from "../src/components/readable-component";
 import { useRouter } from "next/router";
 
 export default function IndexPage() {
-  const text = "Într-un mic sat de pe malul unei râuri, trăia o bătrână blândă și iubitoare numită Ana."
 
   const router = useRouter()
 
   const { setColorScheme, clearColorScheme } = useMantineColorScheme();
 
-  const [readableText, setReadableTex] = useState('')
+  const [hoveredStory, setHoveredStory] = useState<string | null>(null);
+
+  const stories = [
+    { name: "Prințesa și bobul de mazăre", route: "/printesa-si-bobul-de-mazare" },
+    { name: "Iepurele și ariciul", route: "/iepurele-si-ariciul" },
+    { name: "Cei trei purceluși", route: "/cei-trei-purcelusi" },
+    { name: "Iepurașul Peter", route: "/iepurasul-Peter" },
+    { name: "Hansel și Gretel", route: "/hansel-si-gretel" },
+  ];
+
+  const handleMouseOver = (storyName: string) => {
+    setHoveredStory(storyName);
+  };
+
+  const handleMouseOut = () => {
+    setHoveredStory(null);
+  };
+
+  const getStoryImageSource = (storyName: string) => {
+    return hoveredStory !== null && hoveredStory === storyName
+      ? `/${storyName.replace(/\s+/g, "-")}.png`
+      : "/MainPageImage.png";
+  };
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px'}}>
       <Button size="xl">Alege povestea pe care vrei să o citești!</Button>
       <div style={{ display: 'flex', justifyContent: 'space-between', width: '90%', marginTop: '80px', marginBottom: '80px' }}>
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-          <Button size="xl" onClick={() => router.push('/printesa-si-bobul-de-mazare')} style={{ marginBottom: '20px', width: '1000px' }}>Prințesa și bobul de mazăre</Button>
-          <Button size="xl" onClick={() => router.push('/iepurele-si-ariciul')} style={{ marginBottom: '20px', width: '1000px' }}>Iepurele și ariciul</Button>
-          <Button size="xl" onClick={() => router.push('/cei-trei-purcelusi')} style={{ marginBottom: '20px', width: '1000px' }}>Cei trei purceluși</Button>
-          <Button size="xl" onClick={() => router.push('/iepurasul-Peter')} style={{ marginBottom: '20px', width: '1000px' }}>Iepurașul Peter</Button>
-          <Button size="xl" onClick={() => router.push('/hansel-si-gretel')} style={{ marginBottom: '20px', width: '1000px' }}>Hansel și Gretel</Button>
+          {stories.map((story) => (
+            <Button
+              key={story.name}
+              size="xl"
+              onMouseOver={() => handleMouseOver(story.name)}
+              onMouseOut={handleMouseOut}
+              onClick={() => router.push(story.route)}
+              style={{ marginBottom: '20px', width: '1000px' }}
+            >
+              {story.name}
+            </Button>
+          ))}
         </div>
-        <img src="/MainPageImage.png" alt="Povesti pentru copii" width="300px" height="300px"></img>
+        <img src={getStoryImageSource(hoveredStory as string)} alt="Povesti pentru copii" width="300px" height="300px"></img>
       </div>
       <div style={{ display: 'flex', justifyContent: 'space-between', width: '80%' }}>
         <Button size="xl" onClick={() => setColorScheme('dark')}>Întunecat</Button>
